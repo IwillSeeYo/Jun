e1f0bc89bcdd1d1063c314a54bb2e0a3331c7f05
IwillSeeYo <88986526+IwillSeeYo@users.noreply.github.com>
2022-04-12 19:51:01 +0300

    Create Program.cs

---
+++ variables/variables/Program.cs
@@ -0,0 +1,17 @@
+﻿class Program {
+
+    public static void main(String[] args) 
+    {
+
+        string name;
+        int age;
+        string location;
+        float health;
+        bool isHunger;
+        sbyte amounMoney;
+        long serialNumber;
+        double price;
+        uint growth;
+        short lengthToRefrigerator;
+    }
+}
